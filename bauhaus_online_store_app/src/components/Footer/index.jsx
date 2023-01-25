cbf39631963dcc450fedf2ed33a4141f1f8a1956
Dumitru Armas <dimaarmas@gmail.com>
2023-01-25 22:03:02 +0100

    project done

--- bauhaus_online_store_app/src/components/Footer/index.jsx
+++ bauhaus_online_store_app/src/components/Footer/index.jsx
@@ -5,29 +5,43 @@ import { WhatsAppOutlined } from '@ant-design/icons'
 
 export default function Footer() {
     return (
-        <footer className={s.footer_section}>
-            <div className={s.footer_block}>
-                <div className={s.contact_block}>
+        <footer className={s.footer_section} id='footer'>
+            <div className={s.footer_item}>
+                <div>
                     <h2>Contacts</h2>
-                    <p>+49 999 999 999</p>
+                    <br />
+                    <p className={s.phone}>+49 999 999 99 99</p>
 
-                    <div className={s.social_media}>
-                        <div className={s.icons}>
+                    <br />
+                    <div className={s.social_media_icons}>
+                        <a href="instagram.com">
                             <InstagramOutlined />
+                        </a>
+
+                        <a href="https://web.whatsapp.com/">
                             <WhatsAppOutlined />
-                        </div>
+                        </a>
+                    </div>
+
+                    <div className={s.social_media_title}>
+                        <p>instagram</p>
+                        <p>whatsapp</p>
                     </div>
                 </div>
 
-
-                <div className={s.address_block}>
-                    <h2>Address</h2>
-                    <p>Kasseler Str. 134</p>
-                    <p>opening time</p>
-                    <p>around the clock</p>
+                <div>
+                    <p className={s.title_address}>Address</p>
+                    <br />
+                    <p className={s.street}>
+                        Kasseler Str. <br /> Bad Emstal 34308
+                    </p>{" "}
+                    <br />
+                    <p className={s.working_text}>Working hours</p>
+                    <p className={s.working_time}> day and night</p>
                 </div>
 
             </div>
+
         </footer>
 
     )
--- bauhaus_online_store_app/src/components/Layout/index.jsx
+++ bauhaus_online_store_app/src/components/Layout/index.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Footer from '../Footer';
 import Nav from "../Nav";
+import s from './index.module.css'
 
 
 export default function Layout() {
     return (
         <div>
             <Nav />
-            <main>
+            <main className={s.main_section}>
                 <Outlet />
             </main>
-            {/* <Footer /> */}
+            <Footer />
         </div>
     )
 }
